56b9c3314931d839599942c16d5f4f0ec4a620b6
a-poor <45295232+a-poor@users.noreply.github.com>
2020-05-30 00:59:20 -0400

    Adding charts to the dashboard

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from dash.dependencies import Input, Output
 
 
 ###############################
-### Load Data & Make Graphs ###
+###        Load Data        ###
 ###############################
 
 # Summary data
@@ -22,13 +22,43 @@ summary = pd.read_csv(
     names=["VALUE"]
 ).iloc[:4,:]
 summary.VALUE = summary.VALUE.astype("int")
-
 # Tests data
 tests = pd.read_csv(
     "https://raw.githubusercontent.com/nychealth/coronavirus-data/master/tests.csv",
     parse_dates=True
 )
 tests.DATE = pd.to_datetime(tests.DATE)
+# Case / Hospital / Deaths
+case_hosp_death = pd.read_csv(
+    "https://raw.githubusercontent.com/nychealth/coronavirus-data/master/case-hosp-death.csv",
+    parse_dates=True
+)
+# Borough
+by_boro = pd.read_csv(
+    "https://raw.githubusercontent.com/nychealth/coronavirus-data/master/by-boro.csv"
+).iloc[:5]
+# Sex
+by_sex = pd.read_csv(
+    "https://raw.githubusercontent.com/nychealth/coronavirus-data/master/by-sex.csv"
+).iloc[:2]
+# Race
+by_race = pd.read_csv(
+    "https://raw.githubusercontent.com/nychealth/coronavirus-data/master/by-race.csv"
+).sort_values("CASE_RATE_ADJ")
+# Poverty
+by_poverty = pd.read_csv(
+    "https://raw.githubusercontent.com/nychealth/coronavirus-data/master/by-poverty.csv"
+)
+# Age
+by_age = pd.read_csv(
+    "https://raw.githubusercontent.com/nychealth/coronavirus-data/master/by-age.csv"
+).iloc[:5]
+
+
+###############################
+###       Make Plots        ###
+###############################
+
 tests_fig = go.Figure()
 tests_fig.add_trace(go.Scatter(
     x=tests.DATE,
@@ -70,35 +100,62 @@ tests_fig.update_layout(
     ]
 )
 
-# Boro data
-boro = pd.read_csv(
-    "https://raw.githubusercontent.com/nychealth/coronavirus-data/master/boro.csv"
-).iloc[:5]
+def make_boro_graph(number,case_rate):
+    col = f"{number.upper()}_{case_rate.upper()}"
 
-def make_boro_graph(actual_count=False):
-    if actual_count:
-        boro_fig = px.bar(
-            boro.sort_values("COVID_CASE_COUNT",ascending=True),
-            x="COVID_CASE_COUNT",
-            y="BOROUGH_GROUP",
-            orientation='h')
-        boro_fig.update_layout(
-            title="COVID-19 Cases by Borough",
-            xaxis_title="Number of Cases",
-            yaxis_title=None)
+    if number == "CASE":
+        axlabel = "Cases"
+    elif number == "HOSPITALIZED":
+        axlabel = "Hospitalizations"
     else:
-        boro_fig = px.bar(
-            boro.sort_values("COVID_CASE_RATE",ascending=True),
-            x="COVID_CASE_RATE",
-            y="BOROUGH_GROUP",
-            orientation='h')
-        boro_fig.update_layout(
-            title="COVID-19 Cases by Borough",
-            xaxis_title="Case Rate (per 100,000 People)",
-            yaxis_title=None)
+        axlabel = "Deaths"
+
+    if case_rate == "RATE":
+        axlabel += " (per 100,000 People)"
+
+    boro_fig = px.bar(
+        by_boro.sort_values(col,ascending=True),
+        x=col,
+        y="BOROUGH_GROUP",
+        orientation='h')
+
+    boro_fig.update_layout(
+        title="COVID-19 Cases by Borough",
+        xaxis_title=axlabel,
+        yaxis_title=None)
+
     return boro_fig
 
-boro_fig = make_boro_graph(False)
+boro_fig = make_boro_graph(
+    "CASE",
+    "RATE"
+)
+
+race_rate_fig = go.Figure(data=[
+    go.Bar(
+        name='Deaths',
+        x=by_race.DEATH_RATE_ADJ,
+        y=by_race.RACE_GROUP,
+        orientation='h',
+        marker_color="#d62728"),
+    go.Bar(
+        name='Hospitalizations',
+        x=by_race.HOSPITALIZED_RATE_ADJ,
+        y=by_race.RACE_GROUP,
+        orientation='h',
+        marker_color="#ff7f0e"),
+    go.Bar(
+        name='Cases',
+        x=by_race.CASE_RATE_ADJ,
+        y=by_race.RACE_GROUP,
+        orientation='h',
+        marker_color="#1f77b4")
+    
+])
+race_rate_fig.update_layout(
+    title="Age-Adjusted COVID-19 Rate by Race (per 100,000 People)",
+    xaxis_title=None
+)
 
 ###############################
 ###      Make the App       ###
@@ -121,8 +178,10 @@ app.layout = html.Div(
             children=html.I(children="by Austin Poor")
         ),
         html.P(
+            children="Information on the COVID-19 outbreak in NYC."
+        ),
+        html.P(
             children=[
-                "Information on the COVID-19 outbreak in NYC. ",
                 "Data courtesy of the NYC Department of Health and Mental Hygiene. ",
                 "GitHub repo can be found ",
                 html.A(children="here",href="https://github.com/nychealth/coronavirus-data"),
@@ -170,21 +229,43 @@ app.layout = html.Div(
         # By borough
         html.H2(children="Cases by Borough"),
         html.P(children=""),
-        dcc.RadioItems(
-                id='boro-graph-val',
-                options=[
-                    {"label":"Actual Cases","value":"COVID_CASE_COUNT"},
-                    {"label":"Cases per 100,000","value":"COVID_CASE_RATE"}
-                ],
-                value='COVID_CASE_RATE',
-                labelStyle={'display': 'inline-block'}
-            ),
         dcc.Graph(
             id="borough-graph",
             figure=boro_fig
-        )
+        ),
+        dcc.RadioItems(
+                id='boro-graph-number',
+                options=[
+                    {"label":"Cases","value":"CASE"},
+                    {"label":"Hospitalizations","value":"HOSPITALIZED"},
+                    {"label":"Deaths","value":"DEATH"}
+                ],
+                value='CASE',
+                labelStyle={'display': 'inline-block'}
+        ),
+        dcc.RadioItems(
+                id='boro-case-rate',
+                options=[
+                    {"label":"Count","value":"COUNT"},
+                    {"label":"Rate","value":"RATE"}
+                ],
+                value='RATE',
+                labelStyle={'display': 'inline-block'}
+        ),
+
+        # By race / ethnicity
+        html.H2(children="Cases by Race"),
+        html.P(
+            children=""
+        ),
+        dcc.Graph(
+            id="race-rate-graph",
+            figure=race_rate_fig
+        ),
     ]
 )
+
+
 
 ###############################
 ###        Callbacks        ###
@@ -192,13 +273,11 @@ app.layout = html.Div(
 
 @app.callback(
     Output(component_id='borough-graph', component_property='figure'),
-    [Input(component_id='boro-graph-val', component_property='value')]
+    [Input(component_id='boro-graph-number', component_property='value'),
+     Input(component_id='boro-case-rate', component_property='value')]
 )
-def update_boro_plot(actual_or_rate):
-    if actual_or_rate == "COVID_CASE_RATE":
-        return make_boro_graph(False)
-    else:
-        return make_boro_graph(True)
+def update_boro_plot(number,case_rate):
+    return make_boro_graph(number,case_rate)
 
 
 
